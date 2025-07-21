dfebf92c7231e16d691cad344c03b40b48217153
leomacedo <leleo_macedo@hotmail.com>
2025-07-21 17:57:18 -0300

    Primeiro commit do app de finanças pessoais

---
+++ App.js
@@ -0,0 +1,20 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>👋 Olá, Breve um novo app de finanças!</Text>
+    </View>
+  );
+}
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
