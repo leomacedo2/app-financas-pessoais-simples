43f33336ef92a07e7f02c80b5b77cf380177ec99
Leo Macedo <leomacedo19@gmail.com>
2025-10-03 01:27:06 -0300

    sandbox: Atualizando commonstyles

--- utils/commonStyles.js
+++ utils/commonStyles.js
@@ -314,11 +314,11 @@ const commonStyles = StyleSheet.create({
   },
 
 
-  // statusToggleLabel: {
-  //   fontSize: 16,
-  //   color: '#333',
-  //   fontWeight: 'bold',
-  // },
+  statusToggleLabel: {
+    fontSize: 16,
+    color: '#333',
+    fontWeight: 'bold',
+  },
 
 
   // NOVO ESTILO PARA TEXTO DE VALOR TOTAL (HomeScreen)
@@ -328,7 +328,7 @@ const commonStyles = StyleSheet.create({
   //   color: '#333',
   // },
 
-  
+
 });
 
 export default commonStyles;
